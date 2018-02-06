aecf3888c7309a569dbf3ba52f7aeb34daae3604
mithunck <mkallatan@CHRYSLERGDC.COM>
2018-02-06 19:54:53 +0530

    Added date to the todo.

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ todomvc.controller('TodoCtrl',function($scope,$firebaseArray){
       };
     firebase.initializeApp(config);
     var database = firebase.database().ref();
+    var currentDate= function(){
+             var today = new Date();
+             var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+             var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+             var dateTime = date+' '+time;
+             return dateTime;
+    }
+
+
     $scope.todos = $firebaseArray(database);
     $scope.newTodo = '';
 
@@ -22,7 +31,8 @@ todomvc.controller('TodoCtrl',function($scope,$firebaseArray){
         }
         $scope.todos.$add({
             title:newTodo,
-            completed:false
+            completed:false,
+            dateTime:currentDate(),
         });
     };
     $scope.removeTodo = function(todo){
